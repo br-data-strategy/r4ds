f792250ebd175f7109c1c3b4f5c0aecbb788f5d3
Lily Sethre-Brink <lsethre-brink@nci.ent>
2025-11-18 16:34:11 -0600

    ch 2 and ch 3 recovered from the nest

--- chapter-02/exercises-2.5.R
+++ chapter-02/exercises-2.5.R
@@ -9,15 +9,43 @@
 # Exercise placeholders below:
 #
 # Exercise 1 ----
-# (Exercise text)
-#
-# Your code below:
+# Why does this code not work?
+
+my_variable <- 10
+my_varıable
+#> Error: object 'my_varıable' not found
+
+# Answer: B/c the second row misspells variable so it can't recall the vector
+
+# Your code below:N/A
 #
 #
 # Exercise 2 ----
-# (Exercise text)
+# Tweak each of the following R commands so that they run correctly:
+
+libary(todyverse)
+
+ggplot(dTA = mpg) + 
+  geom_point(maping = aes(x = displ y = hwy)) +
+  geom_smooth(method = "lm)"
+
 #
 # Your code below:
 #
+library(tidyverse)
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+
+# Exercise 3 ----
+# Press Option + Shift + K / Alt + Shift + K. What happens? How can you get to 
+# the same place using the menus?
+
+# Answer: It shows you the keyboard shortcuts. Go to the help section at the top 
+# of the page and click "Keyboard Shortcute Help."
+
+# Your code below:N/A
 #
-# Continue as needed for the exercises assigned in the syllabus.
+#
