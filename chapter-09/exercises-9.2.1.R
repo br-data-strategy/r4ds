865971d38d0bf73257cc45b6e53ad799ee33c48a
eclyles <elyles@bakerripley.org>
2025-12-04 16:30:44 -0600

    Exercises 9.2.1

--- chapter-09/exercises-9.2.1.R
+++ chapter-09/exercises-9.2.1.R
@@ -6,18 +6,43 @@
 # --- Exercises from R for Data Science (2e) — Section 9.2.1 ---
 # Replace each '# (Exercise text)' line with the full exercise text from https://r4ds.hadley.nz
 #
+# 9.2.1: 1-4
+
 # Exercise placeholders below:
 #
 # Exercise 1 ----
-# (Exercise text)
+# Create a scatterplot of hwy vs. displ where the points are pink filled in triangles.
+
 #
 # Your code below:
+ggplot(mpg, aes(x=hwy,y=displ)) +
+          geom_point(color="pink", shape=17)
 #
-#
-# Exercise 2 ----
-# (Exercise text)
+# Exercise 2 ---- 
+# Why did the following code not result in a plot with blue points?
+
 #
 # Your code below:
+    ggplot(mpg) + 
+          geom_point(aes(x = displ, y = hwy, color = "blue")) #Color should be set outside of the aesthetic mapping.
+
+
+# Exercise 3 ---- 
+# What does the stroke aesthetic do? What shapes does it work with? (Hint: use ?geom_point)
 #
-#
-# Continue as needed for the exercises assigned in the syllabus.
+# Your code below:
+  # Stroke changes width of border
+          
+          ggplot(mpg, aes(x=hwy,y=displ)) +
+            geom_point(shape = 21, colour = "black", fill = "white", size = 5, stroke = 5) 
+
+
+# Exercise 4 ---- 
+# What happens if you map an aesthetic to something other than a variable name, like aes(color = displ < 5)? 
+# Note, you’ll also need to specify x and y.
+
+# Your code below:
+  # Creates logical true/false if display is less than 5 or not
+          
+          ggplot(mpg, aes(x = hwy, y = displ, color = displ < 5)) + 
+            geom_point()
