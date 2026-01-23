06c55a06a7da9460ff87b6b9e3fa131056489c0b
eclyles <elyles@bakerripley.org>
2026-01-23 16:34:43 -0600

    Chapters 16 and 17

--- chapter-16/exercises-16.4.1.R
+++ chapter-16/exercises-16.4.1.R
@@ -3,21 +3,37 @@
 # Source: https://r4ds.hadley.nz/factors.html#exercises-1
 # NOTE: For full exercise text, see the linked R4DS chapter above.
 #
-# --- Exercises from R for Data Science (2e) — Section 16.4.1 ---
+# --- Exercises from R for Data Science (2e) — Section 16.4.1 1-3---
 # Replace each '# (Exercise text)' line with the full exercise text from https://r4ds.hadley.nz
-#
+
+library(forcats)
+library(ggplot2)
+
+
 # Exercise placeholders below:
 #
 # Exercise 1 ----
-# (Exercise text)
+# There are some suspiciously high numbers in tvhours. Is the mean a good summary?
 #
 # Your code below:
-#
-#
+# No the mean would not be a good summary because of outliers
+
+summary(gss_cat$tvhours)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 0.000   1.000   2.000   2.981   4.000  24.000 
+# NA's 10146 
+
+ggplot(gss_cat, aes(x=tvhours)) +
+  geom_histogram()
+
 # Exercise 2 ----
-# (Exercise text)
+# For each factor in gss_cat identify whether the order of the levels is arbitrary or principled.
 #
 # Your code below:
+# All of them are arbitrary except for rincome
 #
+# Exercise 3 ----
+# Why did moving “Not applicable” to the front of the levels move it to the bottom of the plot?
 #
-# Continue as needed for the exercises assigned in the syllabus.
+# Your code below:
+# There is no "after" so it puts it at end
--- chapter-17/exercises-17.2.5.R
+++ chapter-17/exercises-17.2.5.R
@@ -3,21 +3,55 @@
 # Source: https://r4ds.hadley.nz/datetimes.html#exercises
 # NOTE: For full exercise text, see the linked R4DS chapter above.
 #
-# --- Exercises from R for Data Science (2e) — Section 17.2.5 ---
+# --- Exercises from R for Data Science (2e) — Section 17.2.5 1-3 ---
 # Replace each '# (Exercise text)' line with the full exercise text from https://r4ds.hadley.nz
 #
 # Exercise placeholders below:
 #
+
+library(lubridate)
+library(tidyverse)
+library(nycflights13)
+
 # Exercise 1 ----
-# (Exercise text)
+# What happens if you parse a string that contains invalid dates?
+# 
+ymd(c("2010-10-10", "bananas"))
 #
 # Your code below:
-#
-#
+
+# [1] "2010-10-10" NA          
+# Warning message:
+#  1 failed to parse. 
+
+
+
 # Exercise 2 ----
-# (Exercise text)
+# What does the tzone argument to today() do? Why is it important?
 #
 # Your code below:
+#tzone specifies which time zone you would like the current time in. 
+# the value of "today" depends on what time zone you are in
+
+# Exercise 3 ----
+# For each of the following date-times, show how you’d parse it using a readr column specification and a 
+# lubridate function.
+
+d1 <- "January 1, 2010"
+d2 <- "2015-Mar-07"
+d3 <- "06-Jun-2017"
+d4 <- c("August 19 (2015)", "July 1 (2015)")
+d5 <- "12/30/14" # Dec 30, 2014
+t1 <- "1705"
+t2 <- "11:15:10.12 PM"
 #
-#
-# Continue as needed for the exercises assigned in the syllabus.
+# Your code below:
+
+mdy(d1)
+ymd(d2)
+dmy(d3)
+mdy(d4)
+mdy(d5)
+hms(t2)
+
+# couldnt figure out t1
